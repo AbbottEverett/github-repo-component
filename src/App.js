84ade64795c5bab405600e8424b645bb640aef83
S. Everett Abbott <severettabbott@gmail.com>
2017-12-12 11:51:53 -0800

    Added test component, included react-axios and necessary dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Repo from './components/repo.js'
 
 class App extends Component {
+  state = {
+    textContent: 'test'
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,9 +15,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Repo textContent={this.state.textContent}/>
       </div>
     );
   }
---
+++ src/components/repo.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+const Repo = (props) => {
+  return (
+    <p>{props.textContent}</p>
+  );
+}
+
+export default Repo;
